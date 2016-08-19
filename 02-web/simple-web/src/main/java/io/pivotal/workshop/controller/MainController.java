9305aae9dfae85db8f2037fa705a50b437d74a3f
Mike Gehard <mgehard@pivotal.io>
2016-08-19 15:07:48 -0600

    Make package consistent with previous lab

--- 02-web/simple-web/src/main/java/io/pivotal/workshop/controller/MainController.java
+++ 02-web/simple-web/src/main/java/io/pivotal/workshop/controller/MainController.java
@@ -1,4 +1,4 @@
-package io.pivotal.web.controller;
+package io.pivotal.workshop.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,10 +6,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
-	
+
 	@RequestMapping("/")
 	public ModelAndView home() {
 		return new ModelAndView("views/home");
 	}
-	
+
 }
--- 02-web/simple-web/src/test/java/io/pivotal/workshop/SimpleWebApplicationTests.java
+++ 02-web/simple-web/src/test/java/io/pivotal/workshop/SimpleWebApplicationTests.java
@@ -1,4 +1,4 @@
-package io.pivotal.web;
+package io.pivotal.workshop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
