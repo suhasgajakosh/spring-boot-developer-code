77e4f2887547af2ff1ed8160a6a01f18e937d877
Mike Gehard <mgehard@pivotal.io>
2016-08-24 14:20:13 -0600

    Update for REST lab

--- 02-web/code-snippet-manager/src/main/java/io/pivotal/workshop/domain/Snippet.java
+++ 02-web/code-snippet-manager/src/main/java/io/pivotal/workshop/domain/Snippet.java
@@ -1,4 +1,4 @@
-package io.pivotal.web.domain;
+package io.pivotal.workshop.domain;
 
 import java.util.Date;
 
@@ -6,65 +6,48 @@ import java.util.Date;
 //@XmlRootElement
 public class Snippet {
 
-	private String id;
-	private String title;
-	private String code;
-	
-	//Option 2. You can uncomment this as an alternative of the application.properties
-	//@JsonFormat(pattern="yyyy-MM-dd")
-	private Date created;
-	
-	//Option 2. You can uncomment this as an alternative of the application.properties
-	//@JsonFormat(pattern="yyyy-MM-dd")
-	private Date modified;
-	
-	public Snippet(){}
-	
-	public Snippet(String title, String code) {
-		this.id = java.util.UUID.randomUUID().toString();
-		this.title = title;
-		this.code = code;
-		this.created = new Date();
-		this.modified = new Date();
-	}
+    private String id;
+    private String title;
+    private String code;
 
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public String getCode() {
-		return code;
-	}
-	
-	public void setCode(String code) {
-		this.code = code;
-	}
-	
-	public Date getCreated() {
-		return created;
-	}
-	
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-	
-	public Date getModified() {
-		return modified;
-	}
-	
-	public void setModified(Date modified) {
-		this.modified = modified;
-	}
+    //Option 2. You can uncomment this as an alternative of the application.properties
+    //@JsonFormat(pattern="yyyy-MM-dd")
+    private Date created;
+
+    //Option 2. You can uncomment this as an alternative of the application.properties
+    //@JsonFormat(pattern="yyyy-MM-dd")
+    private Date modified;
+
+    public Snippet() {
+    }
+
+    public Snippet(String title, String code) {
+        this.id = java.util.UUID.randomUUID().toString();
+        this.title = title;
+        this.code = code;
+        this.created = new Date();
+        this.modified = new Date();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
 }
