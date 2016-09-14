bd5bb7fceb52075bf9fbd4a4d454803f02a5fff9
dylanham <dham@pivotal.io>
2016-09-14 08:40:08 -0600

    add overide for form auth within extension of webSecurityConfigurerAdapter

---
+++ 06-security/simple-security/src/main/java/io/pivotal/workshop/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package io.pivotal.workshop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    protected void configureUser(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("billy").password("bob").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+    }
+}
