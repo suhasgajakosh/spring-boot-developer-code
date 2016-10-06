2a82a5d50dd8dbf778d0186604538baf694fd262
dylanham <dham@pivotal.io>
2016-10-06 08:58:57 -0600

    create message and queue within the producer application

--- 07-messaging/simple-rpc-producer-rabbit/src/main/java/io/pivotal/workshop/SimpleRpcProducerRabbitApplication.java
+++ 07-messaging/simple-rpc-producer-rabbit/src/main/java/io/pivotal/workshop/SimpleRpcProducerRabbitApplication.java
@@ -1,34 +1,38 @@
 package io.pivotal.workshop;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.pivotal.workshop.domain.Snippet;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
+@EnableScheduling
 public class SimpleRpcProducerRabbitApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(SimpleRpcProducerRabbitApplication.class);
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(SimpleRpcProducerRabbitApplication.class, args);
 	}
 
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Scheduled(fixedDelay = 1000L)
+	public void send() {
+		this.rabbitTemplate.convertAndSend("spring-boot", "Hello World! at " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+	}
+
 	@Bean
-	CommandLineRunner sender(RabbitTemplate rabbitTemplate){
-		return args -> {
-			Snippet snippet = new Snippet("JavaScript: Alert","alert('Hi there!');");
-			ObjectMapper mapper = new ObjectMapper();
-			String result = (String) rabbitTemplate.convertSendAndReceive("snippet", mapper.writeValueAsString(snippet));
-
-			assert result!=null;
-
-			log.info("Result: " + result);
-		};
+	public Queue queue(){
+		return new Queue("spring-boot");
 	}
 }
